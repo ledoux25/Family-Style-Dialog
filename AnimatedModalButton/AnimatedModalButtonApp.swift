ecad539fc746f0b110ec5d9fd73150ba861efb68
ledoux25 <sanguojoseph17@gmail.com>
2025-09-16 16:08:31 -0400

    Initial Commit

---
+++ AnimatedModalButton/AnimatedModalButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedModalButtonApp.swift
+//  AnimatedModalButton
+//
+//  Created by Sanguo Joseph Ledoux on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedModalButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedModalButtonTests/AnimatedModalButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedModalButtonTests.swift
+//  AnimatedModalButtonTests
+//
+//  Created by Sanguo Joseph Ledoux on 9/16/25.
+//
+
+import Testing
+@testable import AnimatedModalButton
+
+struct AnimatedModalButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
